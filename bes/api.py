25dd3fcc73d14efebe491207c9f06e42c236d61a
Guillaume Sautière <guillaume.sautiere@gmail.com>
2021-03-27 16:17:42 +0100

    add readonly flag to create_youtube_api

--- bes/api.py
+++ bes/api.py
@@ -25,23 +25,25 @@ YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 YOUTUBE_CLIENT_SECRETS_FILE = REPO_ROOT / f'{os.getenv("YOUTUBE_CLIENT_SECRETS_FILE")}'
 
-SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/youtube.readonly",
+          "https://www.googleapis.com/auth/youtube"]
 
 
-def create_youtube_api():
+def create_youtube_api(readonly=True):
     # Get credentials and create an API client
+    scopes = SCOPES[:1] if readonly else SCOPES[1:]
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        str(YOUTUBE_CLIENT_SECRETS_FILE), SCOPES)
+        str(YOUTUBE_CLIENT_SECRETS_FILE), scopes)
     credentials = flow.run_console()
     youtube_api = googleapiclient.discovery.build(
         YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
     return youtube_api
 
 
-def get_or_create_youtube_api():
+def get_or_create_youtube_api(readonly=True):
     global YOUTUBE_API
     if YOUTUBE_API is None:
-        YOUTUBE_API = create_youtube_api()
+        YOUTUBE_API = create_youtube_api(readonly)
     return YOUTUBE_API
 
 
